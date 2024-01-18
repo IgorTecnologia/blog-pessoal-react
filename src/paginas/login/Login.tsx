76109e9d00dac819bf884b33ca7e7fa78d6e8b16
IgorTecnologia <iguigu2019@gmail.com>
2024-01-18 13:32:09 -0300

    Login

---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
+const Login = () => {
+  let navigate = useNavigate()
+  return (
+    <div>
+      <h2 className='text-slate-900 text-5x1 m-4'>login</h2>
+      <div>
+        <button type='submit'
+        onClick={() => {navigate('./home')}}
+        className='houver:inderline mx-4'>Login useNavigate
+        </button>
+        <Link to='/home' className='hover:underline mx-4'>Login por link</Link>
+      </div>
+    </div>
+  
+  )
+}
+
+export default Login
