da6f54f52dc605a3ab2772a777461caf3947ffe2
IgorTecnologia <iguigu2019@gmail.com>
2024-01-22 19:20:33 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Navbar from './componentes/navbar/Navbar';
 import Footer from './componentes/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -7,6 +6,12 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './componentes/temas/listaTemas/ListaTemas';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import FormularioTema from './componentes/temas/formularioTema/FormularioTema';
+
+
+
 
 
 function App() {
@@ -21,6 +26,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
